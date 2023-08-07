9bbd2a884b6e8c20d437c41d6654d7dd0a5bcce6
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-07 19:43:10 +0300

    done

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 
 class User(BaseModel):
-    """ Class User that inherit from BaseModel """
+    """ Class User that inherits from BaseModel """
     
     email=""
     password=""
