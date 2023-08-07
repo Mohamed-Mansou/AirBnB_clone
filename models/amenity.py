c332dee4d748c7091624bb39e35c8ebfe601ae31
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-07 19:37:44 +0300

    task

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Amenity Class"""
+
+from models.base_model import BaseModel
+
+""" Parent class BaseModel"""
+
+class Amenity(BaseModel):
+    """ Amenity class that inherit from Parent class BaseModel """
+    
+    name=""
