dcaee76377fc5bfc8b8dfe6b10bac0de446e919a
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-07 20:03:52 +0300

    done

--- models/review.py
+++ models/review.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+""" Review Class"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """ Review Class that inherits from BaseModel Class """
+    
+    place_id=""
+    user_id=""
+    text=""
