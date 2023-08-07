c9f2c580825f9b3303bfac06a890ee7ce9cbc24b
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-07 19:59:13 +0300

    done

--- models/city.py
+++ models/city.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+""" City class"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    
+    """ City class that inherits from BaseModel class """
+    
+    state_id=""
+    name=""
--- models/state.py
+++ models/state.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" State class"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    
+    """ State class that inherits from BaseModel"""
+    
+name=""
