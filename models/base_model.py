7c2f685d41489efdb1ac2e49174f37c2dfb29637
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-07 19:15:55 +0300

    done

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Base model class which is the parent class"""
+
+import models
+from datetime import datetime
+import uuid
+
+class Basemodel:
+    def __init__(self,*args, **kwargs):
+        """ the BaseModel instance """
+        setattr()
+        pass
+
