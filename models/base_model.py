2c1d6f5cc8b29d5bc869733adbb7feb8891cfdb2
sayedabdelal <saidmo19734@gmail.com>
2023-08-09 16:36:56 -0700

    The Base

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,27 @@ import models
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
-    def __init__():
-        pass
+    """Represents the BaseModel of Task"""
+    def __init__(self, *args, **kwargs):
+        """intial a BaseModel """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def save(self):
+        """updates attribute updated_at with the current datetime """
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        """Print: [<class name>] (<self.id>) <self.__dict__> """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
