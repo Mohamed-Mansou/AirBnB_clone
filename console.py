78053db05c004683da81d01ce3d18b10d8c52587
sayedabdelal <saidmo19734@gmail.com>
2023-08-10 06:39:57 -0700

    The frist

---
+++ console.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""The HBnB console."""
+
+import cmd
+import json
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the command interpreter."""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Exit The Program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit The Program useing End-of-File(Ctrl+D)"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        class_name = args[0]
+        if class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = eval(class_name)()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+        elif len(args) < 1:
+            print("** instance id missing **")
+            return
+        pass
+
+    def do_destroy(self, args):
+        """
+        Deletes an instance based on the class name and id
+        """
+        pass
+
+    def do_all(self, args):
+        """Prints all string representation of all"""
+        pass
+
+    def do_update(self, args):
+        """ Updates an instance based on the class name and id"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
