d320f1c78766aedc6e8c9c5888380c3332c40715
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-11 19:07:21 +0300

    done

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,18 @@ from datetime import datetime
 import uuid
 
 class BaseModel:
-    def __init__(self,id,*args, **kwargs):
-        if kwargs:
-            pass
-        self.id=id.uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ re-create an instance with this dictionary representation."""
+        now = datetime.now()
+        self.id = str(uuid.uuid4()) if "id" not in kwargs else kwargs.get("id")
+        self.created_at = now if "created_at" not in kwargs else kwargs.get("created_at")
+        self.updated_at = now if "updated_at" not in kwargs else kwargs.get("updated_at")
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+            else:
+                setattr(self, key, value)
+        models.storage.new(self)
     def __str__(self):
         """ the string representation"""
         return "[{}],({}),{}".format(self.__class__.__name__,self.id,self.__dict__)
