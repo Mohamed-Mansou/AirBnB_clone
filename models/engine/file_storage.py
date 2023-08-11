3a45ae88bd89141802c379d7d3d44f4723a786b7
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-11 19:07:38 +0300

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """  class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances """
+import json
 
 class FileStorage:
     
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects={}
     
     """ Public instance methods"""
@@ -16,9 +17,13 @@ class FileStorage:
 
         obj_id = obj.__class__.__name__ + "." + str(obj.id)
         self.__objects[obj_id] = obj
+        
     def save(self):
-        """ serializes __objects to the JSON file """
-        pass
+        """Serialize __objects to the JSON file __file_path."""
+        with open(self.__file_path, 'w') as f:
+            json.dump([obj.to_dict() for obj in self.__objects.values()], f)
+
+    
     def reload(self):
         """ deserializes the JSON file to __objects """
         pass
