7a4b38c950457f4999fa070e4bfc8207197c5a87
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-12 21:14:00 +0300

    done

--- models/base_model.py
+++ models/base_model.py
@@ -44,13 +44,14 @@ class BaseModel:
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        
+
         """updates the public instance attribute updated_at
         with the current datetime"""
 
         self.updated_at = datetime.now()
         """ import storage variable """
         models.storage.save()
+
     def to_dict(self):
         """  returns a dictionary containing all keys/
         values of __dict__ of the instance """
