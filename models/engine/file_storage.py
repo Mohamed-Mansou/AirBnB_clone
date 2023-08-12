6bf96a19c216ae20f5f042692d981cde7ce467c9
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-12 21:21:02 +0300

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances """
+"""  class FileStorage that serializes instances to a
+JSON file and deserializes JSON file to instances """
 import json
 import os
 import sys
@@ -11,13 +12,14 @@ from models.review import Review
 from models.user import User
 from models.state import State
 
+
 class FileStorage:
-    
+
     __file_path = 'file.json'
-    __objects={}
-    
+    __objects = {}
+
     """ Public instance methods"""
-    
+
     def all(self):
         """ returns the dictionary of__objects """
         return self.__objects
@@ -26,16 +28,14 @@ class FileStorage:
 
         obj_id = obj.__class__.__name__ + "." + str(obj.id)
         self.__objects[obj_id] = obj
-        
+
     def save(self):
         obj_dict = {}
-
         for key, obj in self.__objects.items():
             obj_dict[key] = obj.to_dict()
         with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
-    
     def reload(self):
         try:
             with open(self.__file_path, 'r') as f:
